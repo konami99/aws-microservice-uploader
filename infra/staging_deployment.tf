d55382ed01f3535fc6c37aa4d1afb81e8386c5c1
Richard Chou <konami99@hotmail.com>
2021-11-25 04:00:29 +1100

    added SNS

--- infra/staging_deployment.tf
+++ infra/staging_deployment.tf
@@ -28,3 +28,31 @@ resource "aws_s3_bucket" "uploader" {
 
   tags = local.common_tags
 }
+
+resource "aws_sns_topic" "s3-event-notification" {
+  name = "s3-event-notification-topic"
+
+  policy = <<POLICY
+{
+    "Version":"2012-10-17",
+    "Statement":[{
+        "Effect": "Allow",
+        "Principal": { "Service": "s3.amazonaws.com" },
+        "Action": "SNS:Publish",
+        "Resource": "arn:aws:sns:*:*:s3-event-notification-topic",
+        "Condition":{
+            "ArnLike":{"aws:SourceArn":"${aws_s3_bucket.uploader.arn}"}
+        }
+    }]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.uploader.id
+
+  topic {
+    topic_arn = aws_sns_topic.s3-event-notification.arn
+    events    = ["s3:ObjectCreated:*"]
+  }
+}
